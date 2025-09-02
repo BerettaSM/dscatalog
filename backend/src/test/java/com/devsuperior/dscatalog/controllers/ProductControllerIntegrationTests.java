b941a6b6ba7546fb37831123500fd5798727ae03
Ramon Saviato <ramonsaviato@hotmail.com>
2025-09-02 17:19:42 -0300

    add more integration tests

--- backend/src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerIntegrationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerIntegrationTests.java
@@ -12,6 +12,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.domain.dto.ProductDTO;
+import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Transactional
@@ -19,6 +23,11 @@ public class ProductControllerIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private ProductDTO productDTO;
 
     private long existingId;
     private long nonExistingId;
@@ -29,6 +38,8 @@ public class ProductControllerIntegrationTests {
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 25L;
+
+        productDTO = Factory.createProductDTO();
     }
 
     @Test
@@ -44,6 +55,41 @@ public class ProductControllerIntegrationTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("Macbook Pro"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value("PC Gamer"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", nonExistingId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productDTO)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void updateShouldReturnOkWhenIdExists() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", existingId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productDTO)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void updateShouldReturnUpdatedProductDTOWhenIdExists() throws Exception {
+
+        String expectedName = productDTO.getName();
+        String expectedDescription = productDTO.getDescription();
+        String expectedImgUrl = productDTO.getImgUrl();
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", existingId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productDTO)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(existingId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(expectedName))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value(expectedDescription))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.imgUrl").value(expectedImgUrl));
     }
 
 }
